928af8b89e9bc5061aeb5e1e931debcc1bcf1db6
geraJuarez <geracjuarez@gmail.com>
2019-03-06 09:52:38 -0600

    add name field

--- models/guerateam.go
+++ models/guerateam.go
@@ -3,6 +3,7 @@ package models
 // Guerateam model
 type Guerateam struct {
 	ID          string   `json:"id"`
+	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	OwnerID     string   `json:"ownerId"`
 	MembersID   []string `json:"membersId"`
