b9b87891ca7a250312547fb140969be4189d9243
geraJuarez <geracjuarez@gmail.com>
2019-04-11 12:28:15 -0500

    change run setup

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gueradevelopment/team-context/routers"
 )
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	r := routers.GetRouter()
-	http.ListenAndServe(":8080", r)
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, r); err != nil {
+		panic(err)
+	}
 }
